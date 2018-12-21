b77186125c25e1e18f6134854dc0bcdd98f9cce6
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2018-12-21 00:46:33 -0500

    Added convenience scripts for running Hugo

---
+++ run_hugo.bat
@@ -0,0 +1,1 @@
+docker run -it --rm -p 1313:1313 -p 1314:1314 -v "$(pwd)":"/usr/share/site" cds-snc/digital-canada-ca
---
+++ run_hugo.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker run -it --rm -p 1313:1313 -p 1314:1314 -v "$(pwd)":"/usr/share/site" cds-snc/digital-canada-ca
