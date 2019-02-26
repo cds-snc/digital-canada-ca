17f1c7fea35644df593be95eb42cce97955decc6
Tim Arney <tim@line37.com>
2019-02-26 09:48:12 -0500

    serve fr dir

--- stage-server.js
+++ stage-server.js
@@ -1,8 +1,12 @@
 const port = parseInt(process.env.PORT, 10) || 4000;
+const path = require('path');
 const express = require('express');
 const server = express();
-server.use(express.static('public/en'));
+//server.use(express.static("public/fr"));
+server.use('/', express.static(path.join(__dirname, "public/en")));
+server.use('/fr', express.static(path.join(__dirname, "public/fr")));
 server.listen(port, err => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
 });
+
