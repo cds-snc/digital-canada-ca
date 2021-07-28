af11f9f42a40f07ed84a863ddd17cc00c4c3a4b4
omartehsin1 <>
2021-07-28 17:03:39 -0400

    WIP

--- static/js/search.js
+++ static/js/search.js
@@ -1,17 +1,17 @@
-
 // window.addEventListener("DOMContentLoaded", function (event) {
 
 // });
 
-
-
 let pagesIndex, searchIndex;
+const MAX_SUMMARY_LENGTH = 100;
+const SENTENCE_BOUNDARY_REGEX = /\b\.\s/gm;
+const WORD_REGEX = /\b(\w*)[\W|\s|\b]?/gm;
 async function initSearchIndex() {
   try {
     const response = await fetch("/index.json");
 
     pagesIndex = await response.json();
-    
+
     searchIndex = lunr(function () {
       this.field("title");
       this.field("author");
@@ -61,7 +61,52 @@ function handleSearchQuery(event) {
     return;
   }
   const results = searchSite(query);
-  console.log("results", results);
+  
+
+  const searchResults = document.getElementById('results')
+  const pTag = document.getElementById('test');
+
+  let resultList = ''
+  for (var result of results) {
+    // resultList += '<li><h2>' + result.title + '</h2>'
+    // resultList += '<p>' + result.author + '...</p></li>'
+    // pTag.innerHTML = result.author
+  }
+  results.map((hit) => {
+    pTag.innerHTML = hit.author;
+    // resultList += '<li><h2>' + result.title + '</h2>'
+    // resultList += '<p>' + result.author + '...</p></li>'
+  })
+
+  // searchResults.innerHTML = resultList;
+  // var target = document.querySelector(".main-inner");
+
+  // while (target.firstChild) {
+  //   target.removeChild(target.firstChild);
+  // }
+
+  // var title = document.createElement("h1");
+  // title.id = "search-results";
+  // title.className = "list-title";
+
+  // if (results.length == 0) title.textContent = `No results found for “${query}”`;
+  // else if (results.length == 1)
+  //   title.textContent = `Found one result for “${query}”`;
+  // else title.textContent = `Found ${results.length} results for “${query}”`;
+  // target.appendChild(title);
+  // document.title = title.textContent;
+
+  // var template = document.getElementById("search-result");
+
+  // for (var result of results) {
+  //   var element = template.content.cloneNode(true);
+  //   element.querySelector(".summary-title").textContent = result.title;
+  //   target.appendChild(element);
+  //   console.log(result.title);
+  // }
+
+
+
   if (!results.length) {
     displayErrorMessage("Your search returned no results");
     return;
@@ -85,8 +130,7 @@ function searchSite(query) {
   const originalQuery = query;
   query = getLunrSearchQuery(query);
   let results = getSearchResults(query);
-  console.log('serres', results);
-  
+
   return results.length
     ? results
     : query !== originalQuery
@@ -95,9 +139,7 @@ function searchSite(query) {
 }
 
 function getSearchResults(query) {
-  
   return searchIndex.search(query).flatMap((hit) => {
-    
     if (hit.ref == "undefined") return [];
     let pageMatch = pagesIndex.filter((page) => page.href === hit.ref)[0];
     pageMatch.score = hit.score;
