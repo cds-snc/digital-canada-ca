a4d20df56596b11ea6ee7a8e137525b14ea0702c
omartehsin1 <>
2021-08-18 12:51:59 -0400

    wip

--- static/js/search.js
+++ static/js/search.js
@@ -1,19 +1,13 @@
-// window.addEventListener("DOMContentLoaded", function (event) {
-
-// });
 
 let pagesIndex, searchIndex;
-const MAX_SUMMARY_LENGTH = 100;
-const SENTENCE_BOUNDARY_REGEX = /\b\.\s/gm;
-const WORD_REGEX = /\b(\w*)[\W|\s|\b]?/gm;
 async function initSearchIndex() {
   
   try {
     const response = await fetch("/index.json");
 
     pagesIndex = await response.json();
-
-    // console.log('response', pagesIndex);
+    console.log(pagesIndex);
+    
 
     searchIndex = lunr(function () {
       this.field("title");
@@ -21,112 +15,79 @@ async function initSearchIndex() {
       this.ref("href");
       pagesIndex.forEach((page) => this.add(page));
     });
+
+    // console.log('SEARCH', searchIndex)
+
     
   } catch (e) {
     console.log(e);
   }
+  // var searchTerm = getQueryVariable("q")
+  // let results = searchIndex.search(searchTerm);
+  // console.log('results', results)
+  
 }
+
 
 
 initSearchIndex();
 
-document.addEventListener("DOMContentLoaded", function () {
-  if (document.getElementById("search-form") != null) {
-    const searchInput = document.getElementById("search");
-    searchInput.addEventListener("keydown", (event) => {
-      if (event.keyCode == 13) handleSearchQuery(event);
-    });
-    document
-      .querySelector(".fa-search")
-      .addEventListener("click", (event) => handleSearchQuery(event));
-    document
-      .querySelectorAll(".clear-search-results")
-      .forEach((button) =>
-        button.addEventListener("click", () => handleClearSearchButtonClicked())
-      )
-  }
-});
-
-function handleSearchQuery(event) {
-  event.preventDefault();
-  const query = document.getElementById("search").value.trim().toLowerCase();
-  
-  if (!query) {
-    displayErrorMessage("Please enter a search term");
-    return;
-  }
-  const results = searchSite(query);
-  results.sort(function(a,b){
-    return new Date(b.date) - new Date(a.date);
-  } )
-
-
-  
-  
-  
-
-  if (!results.length) {
-    displayErrorMessage("Your search returned no results");
-    return;
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split("=");
+      if (pair[0] === variable) {
+          return decodeURIComponent(pair[1].replace(/\+/g, "%20"));
+      }
   }
 }
 
 
+// var resultPages = results.map(function (match) {
+//   return pages[match.ref];
+// });
 
-function clearSearchResults() {
-  const results = document.getElementById('search-result');
-  while (results.firstChild) results.removeChild(results.firstChild);
-
-}
-function handleClearSearchButtonClicked() {
-  clearSearchResults();
-  document.getElementById("search").value = "";
-}
+// console.log(searchIndex);
+// console.log('results', results);
 
 
-
-
-
-function displayErrorMessage(message) {
-  document.querySelector(".search-error-message").innerHTML = message;
-  document.querySelector(".search-container").classList.remove("focused");
-  document.querySelector(".search-error").classList.remove("hide-element");
-  document.querySelector(".search-error").classList.add("fade");
-}
-
-
-
-function searchSite(query) {
+// function handleSearchQuery(event) {
+//   event.preventDefault();
+//   const query = document.getElementById("search").value.trim().toLowerCase();
   
-  const originalQuery = query;
-  query = getLunrSearchQuery(query);
-  let results = getSearchResults(query);
-  console.log('results', results);
+//   const searchResults = document.getElementById("site-results");
+//   console.log("query", getQueryVariable("q"));
+//   if (!query) {
+//     displayErrorMessage("Please enter a search term");
+//     return;
+//   }
+//   const results = searchSite(query);
 
-  return results.length
-    ? results
-    : query !== originalQuery
-    ? getSearchResults(originalQuery)
-    : [];
-}
+//   searchResults.innerHTML = results.map((result) => {
+//     return `
+//     <li>
+//       <p><a href='${result.href}'>${result.title}</a></p>
+//       <p>${result.description}</p>
+//     </li>`
+    
+//   }).join("");
 
-function getSearchResults(query) {
-  return searchIndex.search(query).flatMap((hit) => {
-    if (hit.ref == "undefined") return [];
-    let pageMatch = pagesIndex.filter((page) => page.href === hit.ref)[0];
-    pageMatch.score = hit.score;
-    return [pageMatch];
-  });
-}
+  
+//   // window.location.href = "/search/"
 
-function getLunrSearchQuery(query) {
-  const searchTerms = query.split(" ");
-  if (searchTerms.length === 1) {
-    return query;
-  }
-  query = "";
-  for (const term of searchTerms) {
-    query += `+${term} `;
-  }
-  return query.trim();
-}
+//   console.log('res', results);
+
+
+
+
+  
+  
+  
+
+//   if (!results.length) {
+//     displayErrorMessage("Your search returned no results");
+//     return;
+//   }
+// }
+
