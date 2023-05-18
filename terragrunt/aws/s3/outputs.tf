4e429593903382c0ec6327df4fa2ca3070542023
omartehsin1 <omartehsin@gmail.com>
2023-05-18 13:49:12 -0400

    updated output description

--- terragrunt/aws/s3/outputs.tf
+++ terragrunt/aws/s3/outputs.tf
@@ -1,4 +1,5 @@
 output "s3_bucket_regional_domain_name" {
+  description = "The regional domain names of the buckets that will be used as Cloudfront origin Id"
   value = tomap({
     for k, domain in module.s3_buckets : k => domain.s3_bucket_regional_domain_name
   })
