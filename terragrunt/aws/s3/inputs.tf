1844bc2ce0f10889365ea422293a015fbc737b76
Omar Tehsin <33768337+omartehsin1@users.noreply.github.com>
2023-05-18 15:19:09 -0400

    added s3 module (#4694)
    * added s3 module
    
    * updated plan wf
    
    * updated plan workflow
    
    * updated variable
    
    * updated output description
    
    * terraform fmt
    
    * comment updates
    
    * updated log bucket name

---
+++ terragrunt/aws/s3/inputs.tf
@@ -0,0 +1,5 @@
+variable "buckets" {
+  description = "The two S3 buckets that will hold EN and FR content respectively"
+  type        = set(string)
+  default     = ["cds-website-english-s3-bucket", "cds-website-french-s3-bucket"]
+}
