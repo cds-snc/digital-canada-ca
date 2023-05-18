138e8f4eabe5cfccd5bbd0863ca3dd2a441b9c85
omartehsin1 <omartehsin@gmail.com>
2023-05-18 14:53:15 -0400

    updated log bucket name

--- terragrunt/aws/s3/s3.tf
+++ terragrunt/aws/s3/s3.tf
@@ -12,7 +12,7 @@ module "s3_buckets" {
 
 module "log_bucket" {
   source            = "github.com/cds-snc/terraform-modules?ref=v5.1.8//S3_log_bucket"
-  bucket_name       = "${var.product_name}-${var.env}-logs"
+  bucket_name       = "cds-website-s3-bucket-logs"
   billing_tag_value = var.billing_code
 }
 
