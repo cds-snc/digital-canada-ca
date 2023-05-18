5e825caaa31fd52f668e4d3ab4c0778c11d1a9d1
omartehsin1 <omartehsin@gmail.com>
2023-05-18 13:50:57 -0400

    terraform fmt

--- terragrunt/aws/s3/inputs.tf
+++ terragrunt/aws/s3/inputs.tf
@@ -1,5 +1,5 @@
 variable "buckets" {
   description = "The two S3 buckets that will hold EN and FR content respectively"
-  type    = set(string)
-  default = ["cds-website-english-s3-bucket", "cds-website-french-s3-bucket"]
+  type        = set(string)
+  default     = ["cds-website-english-s3-bucket", "cds-website-french-s3-bucket"]
 }
