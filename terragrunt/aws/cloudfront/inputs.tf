e31907216f91fb8cbf6e89d65507ab34378bc795
omartehsin1 <omartehsin@gmail.com>
2023-06-05 10:34:46 -0400

    added description for var

--- terragrunt/aws/cloudfront/inputs.tf
+++ terragrunt/aws/cloudfront/inputs.tf
@@ -1,4 +1,4 @@
 variable "s3_bucket_regional_domain_name" {
-  description = ""
+  description = "The regional domain names of the buckets that will be used as Cloudfront origin Id"
   type        = map(string)
 }
