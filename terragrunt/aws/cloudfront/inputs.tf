f23b2a4e424e94e6286abb40d719ad6278405b00
omartehsin1 <omartehsin@gmail.com>
2023-06-06 14:06:24 -0400

    added aliases

--- terragrunt/aws/cloudfront/inputs.tf
+++ terragrunt/aws/cloudfront/inputs.tf
@@ -2,3 +2,9 @@ variable "s3_bucket_regional_domain_name" {
   description = "The regional domain names of the buckets that will be used as Cloudfront origin Id"
   type        = map(string)
 }
+
+variable "website_domains" {
+  description = "Website subdomains used for testing purposes"
+  type        = set(string)
+  default     = ["digital.cdssandbox.xyz", "numerique.cdssandbox.xyz"]
+}
--- terragrunt/aws/cloudfront/locals.tf
+++ terragrunt/aws/cloudfront/locals.tf
@@ -1,3 +1,11 @@
 locals {
   cbs_satellite_bucket_arn = "arn:aws:s3:::${var.cbs_satellite_bucket_name}"
+
+  #   cloudfront_variables = flatten([
+  #     for regional_domain_names in var.s3_bucket_regional_domain_name: [
+  #         for site_domains in var.website_domains: {
+  #             s3_regional_domain = "${}"
+  #         }
+  #     ]
+  #   ])
 }
