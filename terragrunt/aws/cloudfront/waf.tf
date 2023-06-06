4de5a9e73706c89354233db217164b6873b6226f
omartehsin1 <omartehsin@gmail.com>
2023-06-06 12:39:28 -0400

    added IAM, UPDATED waf

--- terragrunt/aws/cloudfront/waf.tf
+++ terragrunt/aws/cloudfront/waf.tf
@@ -143,3 +143,39 @@ resource "aws_cloudwatch_log_group" "cds_website_waf" {
     Terraform  = true
   }
 }
+
+resource "aws_kinesis_firehose_delivery_stream" "cds_website_waf" {
+  provider    = aws.us-east-1
+  name        = "aws-waf-log-cds-website"
+  destination = "extended_s3"
+
+  server_side_encryption {
+    enabled = true
+  }
+
+  extended_s3_configuration {
+    role_arn           = aws_iam_role.waf_log_role.arn
+    prefix             = "waf_acl_logs/AWSLogs/${var.account_id}/"
+    bucket_arn         = local.cbs_satellite_bucket_arn
+    compression_format = "GZIP"
+
+    cloudwatch_logging_options {
+      enabled         = true
+      log_group_name  = aws_cloudwatch_log_group.cds_website_waf.name
+      log_stream_name = "WAFLogS3Delivery"
+    }
+  }
+
+  tags = {
+    CostCentre = var.billing_code
+    Terraform  = true
+  }
+
+
+}
+
+resource "aws_wafv2_web_acl_logging_configuration" "cds_website_waf" {
+  provider                = aws.us-east-1
+  log_destination_configs = [aws_kinesis_firehose_delivery_stream.cds_website_waf.arn]
+  resource_arn            = aws_wafv2_web_acl.cds_website_waf.arn
+}
