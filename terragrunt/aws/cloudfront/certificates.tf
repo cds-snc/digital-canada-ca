3da724d11a7f10e9245024621b9916b13d85130f
omartehsin1 <omartehsin@gmail.com>
2023-06-08 10:53:14 -0400

    wip

--- terragrunt/aws/cloudfront/certificates.tf
+++ terragrunt/aws/cloudfront/certificates.tf
@@ -2,7 +2,7 @@ resource "aws_acm_certificate" "cds_website_statement_certificate" {
   for_each                 = var.website_domains
   provider                 = aws.us-east-1
   domain_name              = each.value
-  subject_alternative_name = ["*.${each.value}"]
+  subject_alternative_names = ["*.${each.value}"]
   validation_method        = "DNS"
   tags = {
     CostCentre = var.billing_code
