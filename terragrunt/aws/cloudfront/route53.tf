67c9fb274623e1bca8da4dd7cb9ad665bb279d91
Omar Tehsin <33768337+omartehsin1@users.noreply.github.com>
2023-06-14 10:26:01 -0400

    created alias record (#4725)
    * created alias record
    
    * fmt

---
+++ terragrunt/aws/cloudfront/route53.tf
@@ -0,0 +1,13 @@
+resource "aws_route53_record" "cds_website_A" {
+  for_each = aws_cloudfront_distribution.distribution
+  name     = each.key
+  zone_id  = each.value.hosted_zone_id
+  type     = "A"
+
+  alias {
+    name                   = each.value.domain_name
+    zone_id                = each.value.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
