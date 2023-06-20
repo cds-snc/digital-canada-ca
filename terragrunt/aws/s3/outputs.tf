d61ee3869446b78d89b65a2ef3c222d88c76ef7c
Omar Tehsin <33768337+omartehsin1@users.noreply.github.com>
2023-06-20 14:09:41 -0400

    output s3 modules (#4750)

--- terragrunt/aws/s3/outputs.tf
+++ terragrunt/aws/s3/outputs.tf
@@ -11,3 +11,8 @@ output "s3_bucket_regional_domain_name" {
     for ind, domain in local.regional_domains : local.website_subdomains[ind] => domain.s3_bucket_regional_domain_name
   })
 }
+
+output "s3_buckets" {
+  description = "EN and FR S3 Buckets"
+  value       = module.s3_buckets
+}
