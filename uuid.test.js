d0b1be1e06e9c81fea624370b0911199ef4147a2
omartehsin1 <omartehsin@gmail.com>
2023-07-24 11:25:54 -0400

    updated uuid test

--- uuid.test.js
+++ uuid.test.js
@@ -1,23 +1,27 @@
 const fs = require("fs");
-const filesEN = fs.readdirSync("./content/en/careers/positions/");
-const filesFR = fs.readdirSync("./content/fr/careers/positions/")
 const archFalse = /archived: false/i;
 const UUIDRegex =/[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}/;
 
-test("UUID in correct format EN", () => {
-  filesEN.forEach((element) => {
-    let data = fs.readFileSync(
-      `./content/en/careers/positions/${element}`,
-      "utf8"
-    );
-    if (archFalse.test(data)) {
-      const found = data.match(UUIDRegex);
-      expect(found).not.toBeNull();
-    }
-  });
-});
 
-test("UUID in correct format FR", () => {
+if (fs.readdirSync("./content/en/careers/positions/")) {
+  const filesEN = fs.readdirSync("./content/en/careers/positions/");
+  test("UUID in correct format EN", () => {
+    filesEN.forEach((element) => {
+      let data = fs.readFileSync(
+        `./content/en/careers/positions/${element}`,
+        "utf8"
+      );
+      if (archFalse.test(data)) {
+        const found = data.match(UUIDRegex);
+        expect(found).not.toBeNull();
+      }
+    });
+  });
+}
+
+if (fs.readdirSync("./content/fr/careers/positions/")) {
+  const filesFR = fs.readdirSync("./content/fr/careers/positions/")
+  test("UUID in correct format FR", () => {
     filesFR.forEach((element) => {
     let data = fs.readFileSync(
       `./content/fr/careers/positions/${element}`,
@@ -29,3 +33,4 @@ test("UUID in correct format FR", () => {
     }
   });
 });
+}
